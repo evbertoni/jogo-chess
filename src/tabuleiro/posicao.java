c294dd6e534531947c21cc9fef5eb1bef68fdbd3
Evandro Bertoni <evandro.bertoni@hotmail.com>
2022-02-25 16:18:22 -0300

    adicionado as classes Tabuleiro e Peças

--- src/tabuleiro/posicao.java
+++ src/tabuleiro/posicao.java
@@ -1,5 +1,44 @@
 package tabuleiro;
 
-public class posicao {
+public class Posicao {
+	
+	
+	public Integer linha;
+	public Integer coluna;
+	
+	public Posicao() {
+	}
+	
+	public Posicao(Integer linha, Integer coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+
+	public Integer getLinha() {
+		return linha;
+	}
+
+	public void setLinha(Integer linha) {
+		this.linha = linha;
+	}
+
+	public Integer getColuna() {
+		return coluna;
+	}
+
+	public void setColuna(Integer coluna) {
+		this.coluna = coluna;
+	}
+	
+	public void setPosicao(int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+	
+	@Override
+	public String toString() {
+		return linha +", " + coluna;
+	}
+	
 
 }
